19b2329b6e06e766ee2a2fbfdf50aef3ad5ea109
eveline-hutter <eveline.hutter@students.ffhs.ch>
2019-12-03 21:37:40 +0100

    update main function

--- src/advent/Main.java
+++ src/advent/Main.java
@@ -3,9 +3,10 @@ package advent;
 public class Main {
 
 	public static void main(String[] args) {
-		Puzzle1 puzzle = new Puzzle1();
-		puzzle.sum();
-
+		//Puzzle1 puzzle = new Puzzle1();
+		//puzzle.sum();
+		Puzzle2 puzzle = new Puzzle2(0,0);
+		puzzle.intcode2();
 	}
 
 }
