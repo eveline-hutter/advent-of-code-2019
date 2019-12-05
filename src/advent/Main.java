e6b2473989bf6a651e7a98b6aeef3b954379611a
eveline-hutter <eveline.hutter@students.ffhs.ch>
2019-12-05 06:34:36 +0100

    Update Main.java

--- src/advent/Main.java
+++ src/advent/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		//new Puzzle1().sum();
 		//new Puzzle2(0,0).intcode2();
-		//new Puzzle3().checkPasswords();
+		//new Puzzle4().checkPasswords();
 
 	}
 
